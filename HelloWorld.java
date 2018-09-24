5deadc52a5084f4abe932a5f350bfaaa762b60a4
NghiaLe123 <lenghialk@gmail.com>
2018-09-24 12:30:09 +0700

    master branch revision 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello World I am Nghia");
+		Scanner sc = new Scanner (System.in);
+		String name = sc.nextLine();
+		System.out.println("Hello World I am "+name);
 		
 	}
 	
